b051a66e6126178fc0b38370320c088d74efaee5
João Pedro <jpccordeiro616@gmail.com>
2026-06-22 21:49:18 -0300

    muitas alteracoes funcionando

--- utils/viacep.py
+++ utils/viacep.py
@@ -1,6 +1,21 @@
 import requests
 
-url = f"https://viacep.com.br/ws/<CEP>/json/"
+def buscar_cep(cep):
+    cep = cep.replace("-", "").replace(".", "")
 
-response = requests.get(url)
-dados = response.json()
+    response = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
+
+    if response.status_code == 200:
+        dados = response.json()
+
+        if "erro" in dados:
+            return None
+
+        return {
+            "rua": dados.get("logradouro"),
+            "bairro": dados.get("bairro"),
+            "cidade": dados.get("localidade"),
+            "estado": dados.get("uf")
+        }
+
+    return None
